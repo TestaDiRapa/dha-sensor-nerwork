01d6d607fda185660171efa80446daaab1d6439c
TestaDiRapa <v.c.pierro@gmail.com>
2019-03-17 13:31:32 +0100

    Updated Alivechecker

--- dha-SensorNetwork/src/Server/AliveChecker.java
+++ dha-SensorNetwork/src/Server/AliveChecker.java
@@ -1,28 +1,34 @@
 package Server;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.net.*;
 
 import static Commons.Constants.MAX;
 import static Commons.Constants.PORT;
 import static Commons.ResponseParser.isAlive;
 
+/**
+ * This thread checks for the unicast "ALIVE" messages from the devices. Has an instance of the server
+ */
 public class AliveChecker implements Runnable {
 
     private Server server;
+    private boolean running = true;
 
-    public AliveChecker(Server server) {
+    AliveChecker(Server server) {
         this.server = server;
     }
 
+    /**
+     * Main. Receives a packet and, if it is an "ALIVE" packet, updates the server
+     */
     @Override
     public void run() {
         try(DatagramSocket socket = new DatagramSocket(PORT)){
 
             byte[] buffer = new byte[MAX];
             DatagramPacket bufferPacket = new DatagramPacket(buffer, buffer.length);
-            while(true) {
+            while(running) {
                 socket.receive(bufferPacket);
                 int type = isAlive(bufferPacket);
                 if(type >= 0){
@@ -32,6 +38,19 @@ public class AliveChecker implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    /**
+     * Stops the infinite loop of the main. (It sends a packet to avoid that it waits forever for a packet.
+     */
+    public void closeProtocol() {
+        try(DatagramSocket socket = new DatagramSocket()){
+            running = false;
+            byte[] buffer = "SHUT DOWN".getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("localhost"), PORT);
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
