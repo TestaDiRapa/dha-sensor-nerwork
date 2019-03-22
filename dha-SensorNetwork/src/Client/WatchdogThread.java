653bc88eda7ead8ba8b01eb0d809c4ac534c68de
TestaDiRapa <v.c.pierro@gmail.com>
2019-03-22 14:59:11 +0100

    Added javadoc to WatchdogThread

--- dha-SensorNetwork/src/Client/WatchdogThread.java
+++ dha-SensorNetwork/src/Client/WatchdogThread.java
@@ -1,10 +1,17 @@
 package Client;
 
+/**
+ * This class implements a watchdog that sends a true signal if it doesn't receive a reset every 20 seconds
+ */
 class WatchdogThread {
 
     private Thread watchdog;
     private boolean stop = false;
 
+    /**
+     * Asynchronous method: instantiates a thread that waits 20 seconds. If not interrupt before, changes
+     * the stop variable to true
+     */
     void start() {
         watchdog = new Thread(() -> {
             try {
@@ -17,11 +24,18 @@ class WatchdogThread {
         watchdog.start();
     }
 
+    /**
+     * Interrupts the thread and restart it
+     */
     void restart() {
         watchdog.interrupt();
         start();
     }
 
+    /**
+     * Allow to retrieve the status of the watchdog
+     * @return true if it wasn't reset, false otherwise
+     */
     boolean haveIToStop(){
         return stop;
     }
